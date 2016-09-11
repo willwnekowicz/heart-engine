db016c530428660e860edfc039aff9d64b946c1d
William Wnekowicz <william.wnekowicz@gmail.com>
2016-09-10 18:16:03 -0700

    Add basic layout and elements

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,73 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './index.scss';
 import React from 'react';
+import AppBar from '../node_modules/material-ui/AppBar';
+import Sidebar from './sidebar';
+import { List, ListItem } from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
-        <p>Global bootstrap css import works too as you can see on the following button.</p>
-        <p><a className="btn btn-primary btn-lg">Enjoy!</a></p>
+				<AppBar
+					title="Title"
+				/>
+				<div className={styles.hero}>
+				</div>
+				<div className={styles.sidebar}>
+					<List>
+						<ListItem primaryText="/beat" />
+						<ListItem primaryText="/bpm" />
+						<ListItem primaryText="/variability" />
+						<ListItem primaryText="/nervous" />
+						<ListItem primaryText="/scared" />
+						<ListItem primaryText="/dead" />
+					</List>
+				</div>
+				<div className={styles.main}>
+					<EndpointSection />
+					<EndpointSection />
+					<EndpointSection />
+				</div>
       </div>
     )
   }
+}
+
+class EndpointSection extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>/beat</h1>
+				<p>Global bootstrap css import works too as you can see on the following button.</p>
+				<CodeSnippet />
+				<CodeSnippet />
+			</div>
+		)
+	}
+}
+
+class CodeSnippet extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div className={styles.codeSnippet}>
+				<pre><code>
+					My pre-formatted code
+					here.
+				</code></pre>
+			</div>
+		)
+	}
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import customTheme from './theme'
 
-render(<App/>, document.querySelector("#app"));
+render(
+	<MuiThemeProvider muiTheme={getMuiTheme(customTheme)}>
+		<App/>
+	</MuiThemeProvider>, document.querySelector("#app"));
