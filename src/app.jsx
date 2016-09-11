34e6dc098923ab05d6c27f17c52e55f91d0348f2
William Wnekowicz <william.wnekowicz@gmail.com>
2016-09-10 21:07:56 -0700

    Menu links; request / response show/hide

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import AppBar from '../node_modules/material-ui/AppBar';
 import Sidebar from './sidebar';
 import { List, ListItem } from 'material-ui/List';
 import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import { StickyContainer, Sticky } from 'react-sticky';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -18,24 +19,55 @@ export default class App extends React.Component {
 				/>
 				<div className={styles.hero}>
 				</div>
-				<div className={styles.sidebar}>
-					<List>
-						<ListItem primaryText="/beat" />
-						<ListItem primaryText="/bpm" />
-						<ListItem primaryText="/variability" />
-						<ListItem primaryText="/nervous" />
-						<ListItem primaryText="/scared" />
-						<ListItem primaryText="/dead" />
-					</List>
-				</div>
-				<div className={styles.main}>
-					<EndpointSection />
-					<EndpointSection />
-					<EndpointSection />
-				</div>
-      </div>
+				<StickyContainer>
+					<div className={styles.sidebar}>
+							<Menu />
+					</div>
+					<div className={styles.main}>
+						<EndpointSection name="beat" />
+						<EndpointSection name="bpm" />
+						<EndpointSection name="variability" />
+						<EndpointSection name="nervous" />
+						<EndpointSection name="scared" />
+						<EndpointSection name="dead" />
+					</div>
+				</StickyContainer>
+			</div>
     )
   }
+}
+
+class Menu extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<Sticky className={styles.menu}>
+				<MenuItem name="beat" />
+				<MenuItem name="bpm" />
+				<MenuItem name="variability" />
+				<MenuItem name="nervous" />
+				<MenuItem name="scared" />
+				<MenuItem name="dead" />
+			</Sticky>
+		)
+	}
+}
+
+class MenuItem extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div className={styles.menuItem}>
+				<a href={'#' + this.props.name}>/{this.props.name}</a>
+			</div>
+		)
+	}
 }
 
 class EndpointSection extends React.Component {
@@ -45,11 +77,43 @@ class EndpointSection extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<h1>/beat</h1>
+			<div className={styles.endpointSection}>
+				<a id={this.props.name}></a>
+				<h1>/{this.props.name}</h1>
 				<p>Global bootstrap css import works too as you can see on the following button.</p>
-				<CodeSnippet />
-				<CodeSnippet />
+				<Endpoint />
+			</div>
+		)
+	}
+}
+
+class Endpoint extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			open: false
+		}
+	}
+
+	toggleView() {
+		this.setState({open: !this.state.open})
+	}
+
+	render() {
+		return (
+			<div>
+				<div className={styles.endpoint}
+						 onClick={() => {this.toggleView()}}
+				>
+					GET /beat
+				</div>
+				{ this.state.open ?
+					<div>
+						<CodeSnippet />
+						<CodeSnippet />
+					</div>
+					: null
+				}
 			</div>
 		)
 	}
